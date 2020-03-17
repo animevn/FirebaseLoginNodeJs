da4e8fa24cf61dc5286d4584e65b6492251bc24f
animevn <haanhth21@gmail.com>
2020-03-17 20:25:07 +0700

    sign in with google account and logout

--- functions/index.js
+++ functions/index.js
@@ -46,17 +46,94 @@ app.use(checkIfSignedIn("/"));
 
 app.get("/", (req,res)=>{
   res.render("index");
-})
+});
 
 app.get("/signin", (req, res)=>{
   res.render("signin");
-})
+});
 
 app.get("/register", (req, res)=>{
   res.render("register");
-})
+});
 
+app.get("/google-auth", (req, res)=>{
+  const url = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
 
+    //userinfo.email and userinfo.profile are minimum needed to access user profile
+    scope: ["https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile"]
+  });
+  //redirect to auth url
+  res.redirect(url);
+});
+
+//setCookies for current firebase user
+function setCookies(res){
+  firebase.auth().currentUser.getIdToken().then(idToken=>{
+    //cookie expires in one day
+    const timeOut = 60 * 60 * 24 * 1000;
+    admin.auth().verifyIdToken(idToken).then(decodedClaims=>{
+      //force user to re-signin after 60 minutes
+      if (new Date().getTime()/1000 - decodedClaims.auth_time < 60 * 60) {
+        return admin.auth().createSessionCookie(idToken, {expiresIn: timeOut});
+      }
+      throw new Error("PLEASE SIGN IN AGAIN");
+    }).then(sessionCookie=>{
+      const options = {maxAge:timeOut, httpOnly:true, secure:false};
+      res.cookie("__session", sessionCookie, options);
+      res.redirect("/profile");
+      res.end();
+    }).catch(()=>res.status(401).send("UNAUTHORISED REQUEST"));
+  })
+}
+
+//process redirects from google, /auth/google/callback is defined in google api credential, and
+//can be changed to other name
+app.get("/auth/google/callback", (req, res)=>{
+  const code = req.query;
+  if (code){
+    oAuth2Client.getToken(code, (err, token)=>{
+      if (err){
+        console.log(err);
+        res.redirect("/");
+      }else {
+        let credential = firebase.auth.GoogleAuthProvider.credential(token.id_token);
+        firebase.auth().signInWithCredential(credential).then(()=> setCookies(res))
+        .catch(err=>{
+          console.log(err);
+          res.redirect("/");
+        })
+      }
+    })
+  }
+});
+
+app.get("/profile", (req, res)=>{
+  const sessionCookie = req.cookies.__session || "";
+  admin.auth().verifySessionCookie(sessionCookie, true).then(decodedClaims=>{
+    const uid = decodedClaims.uid;
+    res.render("profile", {userUID:uid});
+  }).catch(err=>{
+    console.log(err);
+    res.redirect("/");
+  })
+});
+
+app.get("/logout", (req, res)=>{
+  const sessionCookie = req.cookies.__session || "";
+  res.clearCookie("__session");
+  if (sessionCookie){
+    admin.auth().verifySessionCookie(sessionCookie, true).then(decodedClaims=>{
+      return admin.auth().revokeRefreshTokens(decodedClaims.sub);
+    }).then(()=> res.redirect("/")).catch(()=>res.redirect("/"));
+  }else {res.redirect("/")}
+});
+
+//redirect wrong routes to index
+app.get("**", (req, res) => {
+  res.status(404).redirect("/");
+});
 
 exports.app = functions.https.onRequest(app);
 
